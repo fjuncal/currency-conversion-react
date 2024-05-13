1ff87a912c99b18cac576a64dd107179c746adde
Fellipe Juncal <fjuncal7@gmail.com>
2024-05-13 16:55:23 -0300

    Implementando a chamada para a api e preenchendo na tela de conversão de moedas. adicionando if e else para mostrar só quando estiver preenchido pela api

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
 import "./CurrencyConverter.css";
+import axios from "axios";
 
 const CurrencyConverter = () => {
+  const [rates, setRates] = useState({});
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://v6.exchangerate-api.com/v6/046985a3ef6eab9b9dff313d/latest/USD"
+      )
+      .then((response) => {
+        setRates(response.data.conversion_rates);
+      })
+      .catch((error) => {
+        console.log("OCORREU UM ERRO: ", error);
+      });
+    console.log(rates.length);
+  }, []);
+
   return (
     <div className="converter">
       <h2>Conversor de Moedas</h2>
@@ -11,10 +33,22 @@ const CurrencyConverter = () => {
       </select>
       <span>para</span>
       <select>
-        <option value="USD">USD</option>
+        {Object.keys(rates).length > 0 ? (
+          Object.keys(rates).map((currency) => (
+            <option value={currency} key={currency}>
+              {currency}
+            </option>
+          ))
+        ) : (
+          <option>loading...</option>
+        )}
       </select>
-      <h3>BRL USD</h3>
-      <p>20 BRL valem 5 USD</p>
+      <h3>
+        {convertedAmount} {toCurrency}
+      </h3>
+      <p>
+        {amount} {fromCurrency} valem {convertedAmount} {toCurrency}
+      </p>
     </div>
   );
 };
